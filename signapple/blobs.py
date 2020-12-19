aeee1d1b24e5fc78b55868dff05abfb508fad941
Andrew Chow <achow101-github@achow101.com>
2020-12-19 13:22:23 -0500

    Fix printing of CodeDirectoryBlob when runtime is null

--- signapple/blobs.py
+++ signapple/blobs.py
@@ -160,7 +160,7 @@ class CodeDirectoryBlob(Blob):
         page_size = 2 ** self.page_size
         s = f"CodeDirectoryBlob: version: {hex(self.version)}; flags: {hex(self.flags)}; hashes: {self.count_code}+{self.count_special}; page size: {page_size}\n"
         s += f"hash size: {self.hash_size}; hash type: {self.hash_type}; platform {self.platform}; code limit 64: {self.code_limit_64}\n"
-        s += f"exec seg base: {self.exec_seg_base}; exec seg limit: {self.exec_seg_limit}; exec seg flags: {self.exec_seg_flags}; runtime: {hex(self.runtime)}\n"
+        s += f"exec seg base: {self.exec_seg_base}; exec seg limit: {self.exec_seg_limit}; exec seg flags: {self.exec_seg_flags}; runtime: {hex(self.runtime) if self.runtime is not None else self.runtime}\n"
         s += f"CodeDirectory Hash: {self.get_hash(self.hash_type).hex()}\n"
         s += f"Identifier: {self.ident}; Team ID {self.team_id}\n"
         if self.ent_der_hash:
