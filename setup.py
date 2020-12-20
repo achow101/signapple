4270685d72cd0cc7f5e9a882da2b925bd41d27c0
Andrew Chow <achow101-github@achow101.com>
2020-12-20 13:22:30 -0500

    Move signapple.py back to codesign.py and add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['signapple']
+
+package_data = \
+{'': ['*'], 'signapple': ['certs/*']}
+
+install_requires = \
+['asn1crypto>=1.4.0,<2.0.0',
+ 'certvalidator @ '
+ 'git+https://github.com/achow101/certvalidator.git@allow-more-criticals',
+ 'macholib>=1.14,<2.0',
+ 'oscrypto>=1.2.1,<2.0.0',
+ 'requests>=2.25.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['signapple = signapple:main']}
+
+setup_kwargs = {
+    'name': 'signapple',
+    'version': '0.1.0',
+    'description': 'Signing and verification tool for MacOS code signatures',
+    'long_description': None,
+    'author': 'Andrew Chow',
+    'author_email': 'achow101-github@achow101.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
