f03df0e17d6cae80bcab13db0ab4ea814d7432fc
Andrew Chow <achow101-github@achow101.com>
2021-01-06 13:34:15 -0500

    Add elfesteem library

--- setup.py
+++ setup.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = \
-['signapple']
+packages = ["signapple"]
 
-package_data = \
-{'': ['*'], 'signapple': ['certs/*']}
+package_data = {"": ["*"], "signapple": ["certs/*"]}
 
-install_requires = \
-['asn1crypto>=1.4.0,<2.0.0',
- 'certvalidator @ '
- 'git+https://github.com/achow101/certvalidator.git@allow-more-criticals',
- 'macholib>=1.14,<2.0',
- 'oscrypto>=1.2.1,<2.0.0',
- 'requests>=2.25.1,<3.0.0']
+install_requires = [
+    "asn1crypto>=1.4.0,<2.0.0",
+    "certvalidator @ "
+    "git+https://github.com/achow101/certvalidator.git@allow-more-criticals",
+    "elf-esteem @ "
+    "git+https://github.com/achow101/elfesteem.git@36f426c5fa5c344620669916e8b723a0b030a3d5",
+    "macholib>=1.14,<2.0",
+    "oscrypto>=1.2.1,<2.0.0",
+    "requests>=2.25.1,<3.0.0",
+]
 
-entry_points = \
-{'console_scripts': ['signapple = signapple:main']}
+entry_points = {"console_scripts": ["signapple = signapple:main"]}
 
 setup_kwargs = {
-    'name': 'signapple',
-    'version': '0.1.0',
-    'description': 'Signing and verification tool for MacOS code signatures',
-    'long_description': None,
-    'author': 'Andrew Chow',
-    'author_email': 'achow101-github@achow101.com',
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': None,
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'entry_points': entry_points,
-    'python_requires': '>=3.8,<4.0',
+    "name": "signapple",
+    "version": "0.1.0",
+    "description": "Signing and verification tool for MacOS code signatures",
+    "long_description": None,
+    "author": "Andrew Chow",
+    "author_email": "achow101-github@achow101.com",
+    "maintainer": None,
+    "maintainer_email": None,
+    "url": None,
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "entry_points": entry_points,
+    "python_requires": ">=3.8,<4.0",
 }
 
 
